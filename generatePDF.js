0ab453e9b1accc7ff117c365a1bda24b5f3cc25b
AhsanMughal <ahsan.emallates@gmail.com>
2020-03-03 23:35:08 +0400

    Add local fonts, remove extra script. fix some issues

--- generatePDF.js
+++ generatePDF.js
@@ -4,10 +4,11 @@ const puppeteer = require("puppeteer");
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     // await page.setViewport({width:1440, height: 900, deviceScaleFactor: 2});
-    await page.goto("file:///D:/temp/html-to-pdf/index.html", { waitUntil: "networkidle2"});
+    await page.goto("file:///D:/personal-github/ahsanmughal.github.io/index.html", { waitUntil: "networkidle2"});
     await page.pdf({
         path: "resume.pdf",
         format: "A4",
+		printBackground : true
     });
 
     await browser.close();
